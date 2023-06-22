e72fd4741234f11408c781e7e3ec32cd4c1ce5dd
tsurui1 <tsurui@creist.co.jp>
2023-06-22 11:34:58 +0900

    article/model追加中

--- article/models.py
+++ article/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField('タイトル', max_length=100)
+    image = models.ImageField('画像')
+    text = models.TextField('テキスト')
+    date_begin = models.DateField('掲載日', blank=True, null=True)
+    date_end = models.DateField('掲載日', blank=True, null=True)
+    state_flag = models.BooleanField('表示', default=True)
