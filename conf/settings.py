60fa63a21064a954a8d98fd4ed69366cc03ad22e
tsurui1 <tsurui@creist.co.jp>
2023-06-22 11:10:29 +0900

    templatesの追加

--- conf/settings.py
+++ conf/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
