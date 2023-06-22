be69f65e6eb7f6847caddfe91ba952a0d66872e1
CREPC0163\ps1101 <a.ogawa@creist.co.jp>
2023-06-22 13:14:25 +0900

    各アプリケーションのテンプレートフォルダ作成

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import redirect, render
+from django.views import generic
+from django.urls import reverse_lazy
 from .models import Schedule
 from .forms import ScheduleForm
+
+
+class ScheduleListView(generic.ListView):
+    model = Schedule
+    template_name = 'schedule_list.html'
