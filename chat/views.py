5a96706d566a6561315b1a8e120e5fd8bd47bdf8
CREPC0163\ps1101 <a.ogawa@creist.co.jp>
2023-06-27 16:41:35 +0900

    base.htmlのnav共通化

--- chat/views.py
+++ chat/views.py
@@ -14,16 +14,17 @@ class ChatView(generic.CreateView):
     template_name = 'chat/chat_top.html'
 
     def dispatch(self, request, *args, **kwargs):
-        """スケジュールメンバーだけが参加できる、メンバー以外はリダイレクト"""
+        """スケジュールメンバーだけが参加できる、メンバー以外はリダイレクト先へ"""
         schedule = Schedule.objects.get(pk=self.kwargs['pk'])
         if self.request.user in schedule.users.all():
             return super().dispatch(request, *args, **kwargs)
         return redirect('schedule:schedule_list')
 
     def get_context_data(self, **kwargs):
-        """フォームで入力したデータを呼び出す + フィルターで絞り込み（スケジュール:detail/pk）"""
+        """テンプレートファイルに渡す + フィルターで絞り込み（スケジュール:detail/pk）"""
         context = super().get_context_data(**kwargs)
-        context['chat_list'] = Chat.objects.filter(groups=Schedule.objects.get(pk=self.kwargs['pk']))
+        context['schedule'] = Schedule.objects.get(pk=self.kwargs['pk'])
+        context['chat_list'] = Chat.objects.filter(groups=Schedule.objects.get(pk=self.kwargs['pk'])).order_by('-created_at')
         return context
 
     def form_valid(self, form):
